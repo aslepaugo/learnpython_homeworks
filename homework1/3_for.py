e9a3373449e0d35a21cde736abeeff0d118a00f6
aslepaugo <aslepaugo@gmail.com>
2020-09-08 19:05:21 +0300

    Style fixes

--- homework1/3_for.py
+++ homework1/3_for.py
@@ -13,23 +13,20 @@ def average_score_report(school):
     school_scores = []
     for school_class in school:
         school_scores += school_class['scores']
-        print("В классе {} средний балл {:.2f}".format(school_class['school_class'], sum(school_class['scores']) / len(school_class['scores'])))
+        print("В классе {} средний балл {:.2f}".format(school_class['school_class'], 
+                                sum(school_class['scores']) / len(school_class['scores'])))
     print('-' * 32)
     print("В школе средний бал {:.2f}".format(sum(school_scores) / len(school_scores)))
 
 
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
     school = [
-        {'school_class': '4а', 'scores': [3,4,5,2,3,4,5,4,3,4,3,3,4,5,3,2,3,4,5,3]},
-        {'school_class': '4б', 'scores': [4,3,3,3,4,4,5,5,4,3,2,3,4,4,3,3,4,4,5,5]},
-        {'school_class': '4в', 'scores': [3,3,3,4,4,4,5,5,5,4,4,3,4,3,3,2,2,3,4,4]},
-        {'school_class': '5а', 'scores': [4,5,4,5,4,4,4,5,5,5,4,3,3,4,5,5,5,4,3,3]},
-        {'school_class': '5б', 'scores': [4,4,4,4,4,4,5,5,5,4,4,5,4,4,3,4,5,4,5,4]},
-        {'school_class': '5в', 'scores': [3,4,5,4,3,3,3,4,4,5,4,4,3,3,3,4,4,5,4,3]},
+        {'school_class': '4а', 'scores': [3, 4, 5, 2, 3, 4, 5, 4, 3, 4, 3, 3, 4, 5, 3, 2, 3, 4, 5, 3]},
+        {'school_class': '4б', 'scores': [4, 3, 3, 3, 4, 4, 5, 5, 4, 3, 2, 3, 4, 4, 3, 3, 4, 4, 5, 5]},
+        {'school_class': '4в', 'scores': [3, 3, 3, 4, 4, 4, 5, 5, 5, 4, 4, 3, 4, 3, 3, 2, 2, 3, 4, 4]},
+        {'school_class': '5а', 'scores': [4, 5, 4, 5, 4, 4, 4, 5, 5, 5, 4, 3, 3, 4, 5, 5, 5, 4, 3, 3]},
+        {'school_class': '5б', 'scores': [4, 4, 4, 4, 4, 4, 5, 5, 5, 4, 4, 5, 4, 4, 3, 4, 5, 4, 5, 4]},
+        {'school_class': '5в', 'scores': [3, 4, 5, 4, 3, 3, 3, 4, 4, 5, 4, 4, 3, 3, 3, 4, 4, 5, 4, 3]},
     ]
 
     average_score_report(school)
