a676f4a25f9ffb9bee140603ab5172f488182512
aslepaugo <aslepaugo@gmail.com>
2020-09-08 08:43:56 +0300

    Fixed IF statement

--- homework1/1_if1.py
+++ homework1/1_if1.py
@@ -16,25 +16,22 @@
 
 def get_responsibility(person_age):
     responsibility = ""
-    if 2 < person_age < 6:
-        responsibility = "Вам нужно быть в саду"
-    elif 5 < person_age < 18:
-        responsibility = "Вы должны учиться в школе"
-    elif 17 < person_age < 23:
-        responsibility = "Вы дожны учиться в ВУЗе"
-    elif 22 < person_age < 71:
-        responsibility = "Вам нужно работать"
-    else:
-        responsibility = "Род занятий не определен. Вы или слишком молоды или уже в преклонном возрасте"
+    
+    if person_age < 3:
+        return "Вы слишком молоды. Оставайтесь дома."
+    if person_age < 6:
+        return "Вам нужно быть в саду"
+    if person_age < 18:
+        return "Вы должны учиться в школе"
+    if person_age < 23:
+        return "Вы дожны учиться в ВУЗе"
+    if person_age < 71:
+        return "Вам нужно работать"
 
-    return responsibility
+    return "Вам пора отдыхать на пенсии"
 
-def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    user_age = input("Please enter your age: ")
+def main(): 
+    user_age = input("Введите Ваш возраст: ")
     user_responsibility = get_responsibility(int(user_age))
     print(user_responsibility)
 
