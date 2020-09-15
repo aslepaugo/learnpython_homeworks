88fe584f4d653043c8d871c032448bb833fd8487
aslepaugo <aslepaugo@gmail.com>
2020-09-15 20:17:02 +0300

    Task 2 completed

--- homework2/2_files.py
+++ homework2/2_files.py
@@ -12,14 +12,24 @@
 4. Замените точки в тексте на восклицательные знаки
 5. Сохраните результат в файл referat2.txt
 """
+import os
 
 
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+
+    if not os.path.exists('referat.txt'):
+        print("Download file from url https://www.dropbox.com/s/sipsmqpw1gwzd37/referat.txt?dl=0")
+        print("and place with this script")
+        return
+
+    with open(r'referat.txt', 'r', encoding='utf-8') as f:
+        content = f.read()
+    print('String lenght: {}'.format(len(content)))
+    print('Words count: {}'.format(len(content.split(" "))))
+    content = content.replace(".", "!")
+
+    with open('referat2.txt', 'w', encoding='utf-8') as f:
+        f.write(content)
 
 
 if __name__ == "__main__":
