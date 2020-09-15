26c2d6aaeba5d62bf8ff7e9f3b55a0b6a8ee6eef
aslepaugo <aslepaugo@gmail.com>
2020-09-15 20:47:32 +0300

    Task 3 complete

--- homework2/3_dict_to_csv.py
+++ homework2/3_dict_to_csv.py
@@ -10,14 +10,32 @@
 2. Запишите содержимое списка словарей в файл в формате csv
 
 """
+import csv
+
+
+def mock_dictionary():
+    return [
+        {'name': 'Gloria', 'age': 23, 'job': 'Juniot Specialist'},
+        {'name': 'Filip', 'age': 32, 'job': 'Supervisor'},
+        {'name': 'Jane', 'age': 34, 'job': 'CEO'},
+        {'name': 'Oskar', 'age': 43, 'job': 'CFO'},
+        {'name': 'Timathy', 'age': 34, 'job': 'Solution Architect'},
+        {'name': 'Luca', 'age': 41, 'job': 'Software Engineer'},
+        {'name': 'Zosja', 'age': 20, 'job': 'Recruiter'},
+        {'name': 'Eliash', 'age': 29, 'job': 'Head of Unit'},
+    ]
 
 
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    people = mock_dictionary()
+    dialect = csv.excel
+    dialect.delimiter = ";"
+    with open('export_people.csv', 'w', encoding='utf-8', newline='') as f:
+        fields = people[0].keys()
+        writer = csv.DictWriter(f, fields, dialect=dialect)
+
+        writer.writeheader()
+        writer.writerows(people)
 
 
 if __name__ == "__main__":
